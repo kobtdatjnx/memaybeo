b12329dc0581528c0c2f117f28c0ae654a648fde
kobtdatjnx <143807887+kobtdatjnx@users.noreply.github.com>
2023-09-04 12:19:27 +0700

    anbabao.lua

---
+++ anbabao.lua
@@ -0,0 +1,21 @@
+spawn(function() 
+    repeat
+        task.wait()
+    until game:IsLoaded()
+    repeat
+        task.wait()
+    until game.Players
+    repeat
+        task.wait()
+    until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
+    wait(1.2)
+    require(game.ReplicatedStorage.Notification).new("<Color=Purple>Nhap type<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Cyan>Abc<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Black>Abc<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Blue>Abc<Color=/>"):Display()
+end)
+spawn(function()
+   loadstring(game:HttpGet(
+	"https://raw.githubusercontent.com/abc/xyz/main/sex.lua
+   ))()
+end)
